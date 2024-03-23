323a495f08a2ca16fcaa4596764f12e294fefc64
imranFRMU <162621539+imranFRMU@users.noreply.github.com>
2024-03-23 00:14:04 -0700

    Add files via upload

---
+++ 123.js
@@ -0,0 +1,34 @@
+// let username = "Bilal"
+// let pw ="456" 
+// if (username=="Bilal" && pw =="456"){
+// console.log("Welcome Home" + " " + username)} else {
+// console.log("bye")}
+// let username = "Bilal"
+// let pw ="789" 
+// if (username=="Bilal" && pw =="456"){
+// console.log("Welcome Home" + " " + username)} else {
+// console.log("Good bye")}
+// Netsted if else
+// let username = "King"
+// let pw = "1978" 
+// let code = "2312"
+// if (username =="King" && pw =="1978"){
+// console.log("Kindly Provide Code")
+// if (code =="2312"){
+// console.log("Welcome Home" + " " + username)
+// }
+// } else {
+// console.log("Good bye")
+// }
+var username = "King";
+var pw = "1978";
+var code = "2312";
+if (username == "King" && pw == "1973") {
+    console.log("Kindly Provide Code");
+    if (code == "2312") {
+        console.log("Welcome Home" + " " + username);
+    }
+}
+else {
+    console.log("Good bye");
+}
---
+++ 123.ts
@@ -0,0 +1,41 @@
+// let username = "Bilal"
+// let pw ="456" 
+// if (username=="Bilal" && pw =="456"){
+// console.log("Welcome Home" + " " + username)} else {
+// console.log("bye")}
+
+
+// let username = "Bilal"
+// let pw ="789" 
+// if (username=="Bilal" && pw =="456"){
+// console.log("Welcome Home" + " " + username)} else {
+// console.log("Good bye")}
+
+// Netsted if else
+
+// let username = "King"
+// let pw = "1978" 
+// let code = "2312"
+
+// if (username =="King" && pw =="1978"){
+// console.log("Kindly Provide Code")
+// if (code =="2312"){
+// console.log("Welcome Home" + " " + username)
+// }
+// } else {
+// console.log("Good bye")
+// }
+
+let username = "King"
+let pw = "1978" 
+let code = "2312"
+
+if (username =="King" && pw =="1973"){
+console.log("Kindly Provide Code")
+if (code =="2312"){
+console.log("Welcome Home" + " " + username)
+}
+} else {
+console.log("Good bye")
+}
+
